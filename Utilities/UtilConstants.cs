ad63d18b338c151c592165ec59b6d670f42fcaa4
Jaden Bottemiller <jadenbottemiller@gmail.com>
2018-01-10 22:20:15 -0800

    Added useful constants to utilities

--- Utilities/UtilConstants.cs
+++ Utilities/UtilConstants.cs
@@ -6,6 +6,19 @@ using System.Threading.Tasks;
 
 namespace Scarlet.Utilities
 {
+    /// <summary>
+    /// Class to store constants
+    /// </summary>
+    public class Constants
+    {
+        // PHYSICAL CONSTANTS
+        public const float GRAVITY = 9.80655f; // meters per second per second
+        public const float EARTH_RADIUS = 6371000; // meters
+
+        // OPERATIONAL CONSTANTS
+        public const int DEFAULT_MIN_THREAD_SLEEP = 15; // milliseconds
+    }
+
     public static class UtilConstants
     {
         public static string SEARCH_NOT_FOUND_STR = "*-+{}SEARCH_NOT_FOUND?/\\";
