d4a84dbc61b14c0aad861052b05156e5e01e2667
Cai Biesinger <Cai.Biesinger@outlook.com>
2018-02-16 15:54:27 -0800

    Continuing work on PCA9685 implementation.

--- Components/Outputs/PCA9685.cs
+++ Components/Outputs/PCA9685.cs
@@ -1,10 +1,15 @@
 ﻿using Scarlet.IO;
+using Scarlet.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Threading;
 
 namespace Scarlet.Components.Outputs
 {
+    /// <summary>
+    /// 16-channel, 12-bit PWM Fm+ I2C bus LED controller.
+    /// Datasheet: https://www.nxp.com/docs/en/data-sheet/PCA9685.pdf
+    /// </summary>
     public class PCA9685
     {
         public class PWMOutputPCA9685 : IPWMOutput
@@ -68,6 +73,7 @@ namespace Scarlet.Components.Outputs
             private void SetConfig()
             {
                 // Full on/off bits
+                // TODO: Is this needed or can I just set full off with invert?
                 bool FullOn = (!this.Enabled && this.Polarity); // Full on only if we are disabled and normally high
                 bool FullOff = (!this.Enabled && !this.Polarity); // Full off only if we are disabled and normally low
                 this.Config[1] = (byte)(FullOn ? (this.Config[1] | 0b0001_0000) : (this.Config[1] & 0b1110_1111)); // Sets full on enable bit
@@ -89,16 +95,27 @@ namespace Scarlet.Components.Outputs
         }
 
         public PWMOutputPCA9685[] Outputs { get; private set; }
+        private PWMOutputPCA9685 AllOutputs;
         private II2CBus Bus;
         private byte PartAddress;
         private int ExtOscFreq;
 
         private const byte FirstModeRegisterOffset = 0x00;
         private const byte FirstLEDRegisterOffset = 0x06;
+        private const byte AllLEDRegisterOffset = 0xFA;
         private const byte PrescaleRegister = 0xFE;
 
-        /// <summary> </summary>
+        // TODO: Implement the following:
+        // - Subaddress set (and use)
+        // - All call set (and use)
+        // - Output change time set
+        // - Invert?
+        // - Output driver settings
+        // - Software reset
+
+        /// <summary> Prepares the PCA9685 device for use. </summary>
         /// <param name="Bus"> The I2C bus that the devie will communicate over. </param>
+        /// <remarks> During setup, there will be quite a bit of communication with the device (70+ bytes). </remarks>
         /// <param name="Address"> The I2C address set via the device's physical address pins. </param>
         /// <param name="ExtOscFreq"> If there is an external oscillator, set the frequency here. If there isn't, set to -1. </param>
         public PCA9685(II2CBus Bus, byte Address, int ExtOscFreq = -1)
@@ -106,29 +123,25 @@ namespace Scarlet.Components.Outputs
             this.Bus = Bus;
             this.PartAddress = Address;
             this.ExtOscFreq = ExtOscFreq;
-            if (ExtOscFreq < -1 || ExtOscFreq > 50000000) { throw new ArgumentException("Ivalid oscillator frequency supplied. Must be bwetween 0 and 50 MHz, or -1 for internal oscillator."); }
+            if (ExtOscFreq < -1 || ExtOscFreq > 50000000) { throw new ArgumentException("Invalid oscillator frequency supplied. Must be bwetween 0 and 50 MHz, or -1 for internal oscillator."); }
             this.Outputs = new PWMOutputPCA9685[16];
             for (byte i = 0; i < this.Outputs.Length; i++) { this.Outputs[i] = new PWMOutputPCA9685(i, this); }
+            this.AllOutputs = new PWMOutputPCA9685(255, this);
             SetupDevice();
-
             ReadAllStates();
         }
 
         private void SetupDevice()
         {
-            // TODO: Implement the following:
-            // - Auto-increment enable
-            // - Subaddress set
-            // - All call set
-            // - Output change time set
-            // - Invert?
-            // - Output driver settings
+            // Enable register auto-increment to make reads/writes much faster
+            byte ModeSettingPre = this.Bus.ReadRegister(this.PartAddress, FirstModeRegisterOffset, 1)[0];
+            byte ModeSettingNew = (byte)((ModeSettingPre & 0b0111_1111) | 0b0010_0000); // Set bit 5 (AI) to 1 to enable auto-increment, but don't set bit 7 (RESET).
+            this.Bus.WriteRegister(this.PartAddress, FirstModeRegisterOffset, new byte[] { ModeSettingNew });
         }
 
         internal void ReadAllStates()
         {
-            this.Bus.Write(this.PartAddress, new byte[] { FirstLEDRegisterOffset }); // Listen up, we're about to grab output state data
-            byte[] OutputData = this.Bus.Read(this.PartAddress, 64); // Gimme all your output state data!
+            byte[] OutputData = this.Bus.ReadRegister(this.PartAddress, FirstLEDRegisterOffset, 64);
             if (OutputData == null || OutputData.Length != 64) { throw new Exception("Reading PCA9685 output state data did not return the correct amount of bytes (64)."); }
             for (int i = 0; i < 16; i++)
             {
@@ -137,9 +150,9 @@ namespace Scarlet.Components.Outputs
             }
         }
 
-        public void SetEnabledAll(bool Enable) { }
+        public void SetEnabledAll(bool Enable) => this.AllOutputs.SetEnabled(Enable);
 
-        public void SetOutputAll(float DutyCycle) { }
+        public void SetOutputAll(float DutyCycle) => this.AllOutputs.SetOutput(DutyCycle);
 
         public void SetFrequency(float Frequency)
         {
@@ -149,6 +162,7 @@ namespace Scarlet.Components.Outputs
             if(TempPrescale < 3) { TempPrescale = 3; }
             if(TempPrescale > 255) { TempPrescale = 255; }
             byte PrescaleVal = (byte)(TempPrescale);
+            Log.Output(Log.Severity.DEBUG, Log.Source.HARDWAREIO, "Setting PCA9685 frequency prescaler value to " + PrescaleVal + ".");
 
             // Put the oscillator in sleep mode.
             byte ModeSettingPre = this.Bus.ReadRegister(this.PartAddress, FirstModeRegisterOffset, 1)[0];
@@ -172,9 +186,34 @@ namespace Scarlet.Components.Outputs
             }
         }
 
-        internal void SetChannelData(int Channel, byte[] Data)
+        /// <summary> Switches the PCA9685 to use the external oscillator. This can only be disabled via a power cycle or software reset. </summary>
+        /// <remarks> External oscillator must be between 0 and 50MHz. </remarks>
+        /// <param name="Frequency"> This is used in software to determine some timing values. Provide the actual frequence of the attached oscillator. </param>
+        public void UseExternalOscillator(int Frequency)
         {
+            if (Frequency < 0 || Frequency > 50000000) { throw new Exception("External oscillator must be between 0 and 50 MHz. Switch not made."); }
+            this.ExtOscFreq = Frequency;
 
+            // Put the internal oscillator into sleep mode.
+            byte ModeSettingPre = this.Bus.ReadRegister(this.PartAddress, FirstModeRegisterOffset, 1)[0];
+            byte ModeSettingNew = (byte)((ModeSettingPre & 0b0111_1111) | 0b0001_0000); // Set bit 4 (SLEEP) to 1 to shut down the oscillator, and also make sure we're not setting bit 7 (RESET).
+            this.Bus.WriteRegister(this.PartAddress, FirstModeRegisterOffset, new byte[] { ModeSettingNew });
+
+            // Now switch to EXTCLK.
+            byte ModeSettingPreExt = this.Bus.ReadRegister(this.PartAddress, FirstModeRegisterOffset, 1)[0];
+            byte ModeSettingNewExt = (byte)((ModeSettingPreExt & 0b0111_1111) | 0b0101_0000); // Set bits 6 and 4 (EXTCLK & SLEEP), but not bit 7 (SLEEP).
+            this.Bus.WriteRegister(this.PartAddress, FirstModeRegisterOffset, new byte[] { ModeSettingNewExt });
+            Thread.Sleep(1); // Probably not needed, but may as well.
+
+        }
+
+        internal void SetChannelData(byte Channel, byte[] Data)
+        {
+            if (Data == null || Data.Length != 4) { throw new Exception("Invalid data being set for channel " + Channel); }
+            byte Register;
+            if (Channel == 255) { Register = AllLEDRegisterOffset; }
+            else { Register = (byte)((Channel * 4) + FirstLEDRegisterOffset); } // Single LED channel
+            this.Bus.WriteRegister(this.PartAddress, Register, Data);
         }
     }
 }
