bf9ce7312620d5623221aac28a6d8a2c79f17b2d
Cai Biesinger <cai.biesinger@outlook.com>
2018-02-17 22:57:13 -0800

    Starting implementation of DataLog system

---
+++ UnitTest/UtilitiesTesting/DataLogTesting.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Scarlet.Utilities;
+using System.Collections.Generic;
+
+namespace UnitTest.UtilitiesTesting
+{
+    [TestClass]
+    public class DataLogTesting
+    {
+        SensorData DataCont = new SensorData()
+        {
+            { "int", 0x811426 },
+            { "float", 1.0486F },
+            { "bool", false },
+            { "Dictionary", new Dictionary<string, string>() },
+            { "string", "Erza" },
+            { "byte", (byte)19 },
+            { "long", 811426811426L }
+        };
+
+        [TestMethod]
+        public void SensorDataStructureTest()
+        {
+            Assert.AreEqual(0x811426, this.DataCont.GetValue<int>("int"));
+            Assert.AreEqual(1.0486F, this.DataCont.GetValue<float>("float"));
+            Assert.AreEqual(false, this.DataCont.GetValue<bool>("bool"));
+            Assert.IsInstanceOfType(this.DataCont.GetValue<Dictionary<string, string>>("Dictionary"), typeof(Dictionary<string, string>));
+            Assert.AreEqual("Erza", this.DataCont.GetValue<string>("string"));
+            Assert.AreEqual((byte)19, this.DataCont.GetValue<byte>("byte"));
+            Assert.AreEqual(811426811426L, this.DataCont.GetValue<long>("long"));
+
+            Assert.ThrowsException<InvalidCastException>(delegate { this.DataCont.GetValue<int>("string"); });
+            Assert.ThrowsException<InvalidCastException>(delegate { this.DataCont.GetValue<bool>("string"); });
+            Assert.ThrowsException<InvalidCastException>(delegate { this.DataCont.GetValue<string>("Dictionary"); });
+            Assert.ThrowsException<InvalidCastException>(delegate { this.DataCont.GetValue<int>("long"); });
+            Assert.ThrowsException<InvalidCastException>(delegate { this.DataCont.GetValue<byte>("int"); });
+            Assert.ThrowsException<InvalidCastException>(delegate { this.DataCont.GetValue<byte>("bool"); });
+            Assert.ThrowsException<InvalidCastException>(delegate { this.DataCont.GetValue<float>("string"); });
+
+            foreach (KeyValuePair<string, object> Item in this.DataCont) // Just to check there are no errors doing this.
+            {
+                Console.WriteLine(String.Format("key \"{0}\" has value \"{1}\".", Item.Key, Item.Value));
+            }
+        }
+    }
+}
---
+++ Utilities/DataLog.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scarlet.Utilities
+{
+    public class DataLog
+    {
+
+    }
+
+    public class SensorData : IEnumerable
+    {
+        private Dictionary<string, object> Data = new Dictionary<string, object>();
+
+        public void Add<DataType>(string key, DataType value)// where DataType : class
+        {
+            this.Data.Add(key, value);
+        }
+
+        public DataType GetValue<DataType>(string key)// where DataType : class
+        {
+            return (DataType)this.Data[key];
+        }
+
+        public IEnumerator GetEnumerator() => this.Data.GetEnumerator();
+    }
+}
