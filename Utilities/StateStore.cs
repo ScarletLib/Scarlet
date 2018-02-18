5fca36590ddb55fef0b9cff9cef759fef5c87f12
Cai Biesinger <cai.biesinger@outlook.com>
2018-02-18 00:47:41 -0800

    Fixed rare Win32 exception in StateStore, and made it more fault resilient.

--- Utilities/StateStore.cs
+++ Utilities/StateStore.cs
@@ -19,6 +19,7 @@ namespace Scarlet.Utilities
         {
             if (Started) { return; }
             FileInfo = new FileInfo( "ScarletStore-" + SystemName + FileExtension);
+            CheckForBackups();
             if(!File.Exists(FileInfo.FullName)) { File.Create(FileInfo.FullName).Close(); }
             Data = new Dictionary<string, string>();
             foreach (string Line in File.ReadAllLines(FileInfo.FullName))
@@ -26,6 +27,23 @@ namespace Scarlet.Utilities
                 Data.Add(Line.Split('=')[0], string.Join("=", Line.Split('=').Skip(1).ToArray()));
             }
             Started = true;
+        }
+
+        private static void CheckForBackups()
+        {
+            FileInfo OldFile = new FileInfo(FileInfo.Name + "-old" + FileExtension);
+            if(File.Exists(OldFile.FullName))
+            {
+                Log.Output(Log.Severity.WARNING, Log.Source.OTHER, "StateStore found a backup file in place, meaning that something went wrong while saving last time. This backup file will now be used.");
+                Log.Output(Log.Severity.WARNING, Log.Source.OTHER, "If this keeps happening, there may be a corruption problem. Check the *.txt-corrupt file to see why this may be.");
+                if (File.Exists(FileInfo.FullName)) // New file exists
+                {
+                    FileInfo CorrFile = new FileInfo(FileInfo.Name + "-corrupt" + FileExtension);
+                    if(File.Exists(CorrFile.FullName)) { File.Delete(CorrFile.FullName); } // If we already have an old new one, get rid of it.
+                    File.Move(FileInfo.FullName, CorrFile.FullName);
+                }
+                File.Move(OldFile.FullName, FileInfo.FullName); // Replace the potentially corrupt file with the backup
+            }
         }
 
         /// <summary> Saves the configuration to disk. </summary>
@@ -37,9 +55,9 @@ namespace Scarlet.Utilities
                 Lines.Add(Item.Key + '=' + Item.Value);
             }
             FileInfo OldFile = new FileInfo(FileInfo.Name + "-old" + FileExtension);
-            File.Move(FileInfo.FullName, OldFile.FullName);
-            File.WriteAllLines(FileInfo.FullName, Lines);
-            File.Delete(OldFile.FullName);
+            File.Move(FileInfo.FullName, OldFile.FullName); // Move the current saved file into a backup
+            File.WriteAllLines(FileInfo.FullName, Lines); // Save the new version
+            File.Delete(OldFile.FullName); // Delete the backup if saving went OK
         }
 
         /// <summary> Sets the specified proprty internally. Does not change the file, you must use Save() to save changes to file. </summary>
