d6264da85a50449ca3004e4caf8bf67f42964417
Cai Biesinger <cai.biesinger@outlook.com>
2018-04-07 20:38:18 -0700

    Finishing VH400, untested, but it should work.

--- Components/Sensors/VH400.cs
+++ Components/Sensors/VH400.cs
@@ -17,10 +17,12 @@ namespace Scarlet.Components.Sensors
         public string System { get; set; }
         private IAnalogueIn Input;
         private double LastReading;
+        public double VoltageMultiplier { get; set; }
 
         public VH400(IAnalogueIn Input)
         {
             this.Input = Input;
+            this.VoltageMultiplier = 1;
         }
 
         /// <summary> Gets the sensor's data in a standard format. </summary>
@@ -29,7 +31,7 @@ namespace Scarlet.Components.Sensors
             return new DataUnit("VH400")
             {
                 { "RawReading", this.LastReading },
-                { "VWC%", ToMoisture(this.LastReading) }
+                { "VWC%", ToMoisture(this.LastReading, this.VoltageMultiplier) }
             }
             .SetSystem(this.System);
         }
@@ -42,7 +44,7 @@ namespace Scarlet.Components.Sensors
 
         /// <summary> Gets the volumetric water content in %, between 0 and 1. </summary>
         /// <returns> VWC %, or -1 if the sensor is not functioning correctly. </returns>
-        public float GetReading() { return ToMoisture(this.LastReading); }
+        public float GetReading() { return ToMoisture(this.LastReading, this.VoltageMultiplier); }
 
         /// <summary> Gets the sensor's output voltage (should be between 0 and 3V). </summary>
         public float GetVoltage() { return (float)this.LastReading; }
@@ -50,9 +52,11 @@ namespace Scarlet.Components.Sensors
         /// <summary> Converts the sensor's voltage output to Volumetric water content (%) between 0 and 1. </summary>
         /// <remarks> Uses the piecewise curve taken from https://vegetronix.com/Products/VH400/VH400-Piecewise-Curve.phtml </remarks>
         /// <param name="RawReading"> The sensor's output voltage (0-3V) </param>
+        /// <param name="VoltageMultipler"> The amount to multiply the input voltage by before making calculations. THis is useful if you have a resistor divider on the input, in which case you'd want a multiplier > 1. </param>
         /// <returns> Volumetric water content % (0-1) if valid input, -1 if invalid input. </returns>
-        public static float ToMoisture(double RawReading)
+        public static float ToMoisture(double RawReading, double VoltageMultipler)
         {
+            RawReading *= VoltageMultipler;
             if (RawReading < 0 || RawReading > 3) { return -1; } // Invalid input.
             if (RawReading < 1.1) { return (float)((0.1 * RawReading) - 0.01); }
             if (RawReading < 1.3) { return (float)((0.15 * RawReading) - 0.175); }
