72584401bfbcee024d329302479e45b33fff9b82
Nathan Wreggit <wreggit@uw.edu>
2018-04-27 15:03:40 -0700

    Implementation of VESC Motor controller and extra utilities

--- Utilities/UtilData.cs
+++ Utilities/UtilData.cs
@@ -152,5 +152,50 @@ namespace Scarlet.Utilities
             return (ushort)(((Input & 0b1111_1111) << 8) | ((Input >> 8) & 0b1111_1111));
         }
 
+        
+        // Generates a CRC 16 Checksum
+        public static class Crc16
+        {
+            const ushort polynomial = 0xA001;
+            static readonly ushort[] table = new ushort[256];
+
+            /// <summary> Computes a CRC-16 checksum from given payload. </summary>
+            /// <param name="bytes"> Payload of which to compute the checksum </param>
+            public static ushort ComputeChecksum(byte[] bytes)
+            {
+                ushort crc = 0;
+                for (int i = 0; i < bytes.Length; ++i)
+                {
+                    byte index = (byte)(crc ^ bytes[i]);
+                    crc = (ushort)((crc >> 8) ^ table[index]);
+                }
+                return crc;
+            }
+
+            static Crc16()
+            {
+                ushort value;
+                ushort temp;
+                for (ushort i = 0; i < table.Length; ++i)
+                {
+                    value = 0;
+                    temp = i;
+                    for (byte j = 0; j < 8; ++j)
+                    {
+                        if (((value ^ temp) & 0x0001) != 0)
+                        {
+                            value = (ushort)((value >> 1) ^ polynomial);
+                        }
+                        else
+                        {
+                            value >>= 1;
+                        }
+                        temp >>= 1;
+                    }
+                    table[i] = value;
+                }
+            }
+        }
+
     }
 }
