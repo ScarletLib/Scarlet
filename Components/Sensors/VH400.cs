3be7df89821b7309d9bc36cf8aea86f39d73b401
Cai Biesinger <cai.biesinger@outlook.com>
2018-05-12 21:47:34 -0700

    Fixing voltage multiplier implementation.

--- Components/Sensors/VH400.cs
+++ Components/Sensors/VH400.cs
@@ -17,12 +17,19 @@ namespace Scarlet.Components.Sensors
         public string System { get; set; }
         private IAnalogueIn Input;
         private double LastReading;
-        public double VoltageMultiplier { get; set; }
+        public double VoltageMultiplier;
 
-        public VH400(IAnalogueIn Input)
+        /// <summary> Prepares a VH400 soil moisture sensor for use. </summary>
+        /// <param name="Input"> The analogue input where the sensor is connected. </param>
+        /// <param name="VoltageMultiplier">
+        /// If a resistor divider or other scaling is applied to the sensor's output, specify this parameter in order to scale the readings to the appropriate levels.
+        /// For example, if there is a 3:1 divider, bringing the sensor's 0-3V output to a range of 0-1V on the IAnalogeIn device, then specify a multiplier of 3.
+        /// This would be decided by the hardware configuration of the sensor and analogue input systems, and varies by implementation.
+        /// </param>
+        public VH400(IAnalogueIn Input, double VoltageMultiplier = 1)
         {
             this.Input = Input;
-            this.VoltageMultiplier = 1;
+            this.VoltageMultiplier = VoltageMultiplier;
         }
 
         /// <summary> Gets the sensor's data in a standard format. </summary>
@@ -31,7 +38,8 @@ namespace Scarlet.Components.Sensors
             return new DataUnit("VH400")
             {
                 { "RawReading", this.LastReading },
-                { "VWC%", ToMoisture(this.LastReading, this.VoltageMultiplier) }
+                { "Multiplier", this.VoltageMultiplier },
+                { "VWC%", ToMoisture(this.LastReading) }
             }
             .SetSystem(this.System);
         }
@@ -40,11 +48,11 @@ namespace Scarlet.Components.Sensors
         public bool Test() => (this.LastReading >= 0 && this.LastReading <= 3);
 
         /// <summary> Gets a new reading from the sensor. </summary>
-        public void UpdateState() { this.LastReading = this.Input.GetInput(); }
+        public void UpdateState() { this.LastReading = this.Input.GetInput() * this.VoltageMultiplier; }
 
         /// <summary> Gets the volumetric water content in %, between 0 and 1. </summary>
         /// <returns> VWC %, or -1 if the sensor is not functioning correctly. </returns>
-        public float GetReading() { return ToMoisture(this.LastReading, this.VoltageMultiplier); }
+        public float GetReading() { return ToMoisture(this.LastReading); }
 
         /// <summary> Gets the sensor's output voltage (should be between 0 and 3V). </summary>
         public float GetVoltage() { return (float)this.LastReading; }
@@ -52,11 +60,9 @@ namespace Scarlet.Components.Sensors
         /// <summary> Converts the sensor's voltage output to Volumetric water content (%) between 0 and 1. </summary>
         /// <remarks> Uses the piecewise curve taken from https://vegetronix.com/Products/VH400/VH400-Piecewise-Curve.phtml </remarks>
         /// <param name="RawReading"> The sensor's output voltage (0-3V) </param>
-        /// <param name="VoltageMultipler"> The amount to multiply the input voltage by before making calculations. THis is useful if you have a resistor divider on the input, in which case you'd want a multiplier > 1. </param>
         /// <returns> Volumetric water content % (0-1) if valid input, -1 if invalid input. </returns>
-        public static float ToMoisture(double RawReading, double VoltageMultipler)
+        public static float ToMoisture(double RawReading)
         {
-            RawReading *= VoltageMultipler;
             if (RawReading < 0 || RawReading > 3) { return -1; } // Invalid input.
             if (RawReading < 1.1) { return (float)((0.1 * RawReading) - 0.01); }
             if (RawReading < 1.3) { return (float)((0.15 * RawReading) - 0.175); }
