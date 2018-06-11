7812c6df26c7c5b46e387957d7d4b547a509d166
Cai Biesinger <cai.biesinger@outlook.com>
2018-06-10 19:01:39 -0700

    Beginning MTK3339 implementation. Checksum works.

--- Components/Sensors/MTK3339.cs
+++ Components/Sensors/MTK3339.cs
@@ -9,111 +9,58 @@ namespace Scarlet.Components.Sensors
 {
     public class MTK3339 : ISensor
     {
-        public float Latitude { get; private set; }
-        public float Longitude { get; private set; }
         public string System { get; set; }
 
         private IUARTBus UART;
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="T:Scarlet.Components.Sensors.MTK3339"/> class. 
-        /// Sets it to send GPRMC and GPGGA every 200 milliseconds. 
-        /// </summary>
-        /// <param name="UART"> The UART bus to read from and write to. </param>
         public MTK3339(IUARTBus UART)
         {
-            this.UART = UART ?? throw new Exception("Cannot initialize MTK3339 with null UART bus!");
-            if (UART.BytesAvailable() > 0) { UART.Read(UART.BytesAvailable(), new byte[UART.BytesAvailable()]); }
-            Thread ParseThread = new Thread(Parse) { IsBackground = true };
-            ParseThread.Start();
+            
+            
         }
 
-        private void Parse()
+        private void InterpretPacket()
         {
-            StringBuilder Builder = new StringBuilder();
 
-            while (true)
+        }
+
+        private bool CheckPacket()
+        {
+            return false;
+        }
+
+        /// <summary> Calculates the checksum of a given packet. </summary>
+        /// <param name="PacketContents"> The packet to calculate the checksum of. </param>
+        /// <param name="UseFull"> If false, then only text between '$' and '*' is taken into account. If true, the entire string is used. </param>
+        /// <returns> A 1-byte standard NMEA checksum of the packet contents, or null if the packet is not valid. </returns>
+        private byte? GetChecksum(string PacketContents, bool UseFull = false)
+        {
+            if (string.IsNullOrEmpty(PacketContents)) { return null; }
+            string ToCheck = PacketContents;
+            if (!UseFull)
             {
-                char Data = BlockingGetChar();
-                Builder.Append(Data);
-                if (Data == '\n')
-                {
-                    if (Builder.ToString().Contains("$GPGGA"))
-                    {
-                        int? LatIndex = null;
-                        int? LngIndex = null;
-                        string[] SplitData = Builder.ToString().Split(',');
-                        for (int i = 0; i < SplitData.Length; i++)
-                        {
-                            switch (SplitData[i])
-                            {
-                                case "N": LatIndex = i - 1; break;
-                                case "S": LatIndex = -(i - 1); break;
-                                case "W": LngIndex = -(i - 1); break;
-                                case "E": LngIndex = i - 1; break;
-                            }
-                        }
-
-                        if (LatIndex.HasValue && LngIndex.HasValue)
-                        {
-                            this.Latitude = RawToDeg(SplitData[Math.Abs(LatIndex.Value)]) * Math.Sign(LatIndex.Value);
-                            this.Longitude = RawToDeg(SplitData[Math.Abs(LngIndex.Value)]) * Math.Sign(LngIndex.Value);
-                        }
-                    }
-                    Builder.Clear();
-                }
+                int Start = PacketContents.IndexOf('$');
+                int End = PacketContents.IndexOf('*');
+                if (Start == -1 || End == -1) { return null; }
+                ToCheck = PacketContents.Substring(Start + 1, (End - (Start + 1)));
             }
+
+            byte Checksum = 0;
+            for (int i = 0; i < ToCheck.Length; i++) { Checksum ^= Convert.ToByte(ToCheck[i]); }
+            return Checksum;
         }
 
-        private char BlockingGetChar()
-        {
-            byte[] NewChar = new byte[1];
-            while (this.UART.BytesAvailable() < 1) { Thread.Sleep(10); }
-            this.UART.Read(1, NewChar);
-            return (char)NewChar[0];
-        }
+        public bool Test() => false;//HasFix();
 
-        /// <summary> Checks whether this GPS has a fix. </summary>        
-        /// <returns> Returns true if the GPS has a fix and false otherwise. </returns>
-        public bool Test() => HasFix();
-
-        /// <summary> Gets new readings from GPS. </summary>
-        public void UpdateState() => GetCoords();
-
-        /// <summary> Queries the GPS to see if it has a fix. </summary>
-        /// <returns> <c>true</c>, if fix was hased, <c>false</c> otherwise. </returns>
-        [Obsolete("This currently does not actually check if a fix is hased, and will always return true.")]
-        public bool HasFix()
-        {
-            return true;
-        }
-
-        /// <summary> Gets the GPS coordinates of this GPS. </summary>
-        /// <returns> Returns a tuple with the GPS coordinates, with Latitude first and Longitude second. </returns>
-        public Tuple<float, float> GetCoords()
-        {
-            return new Tuple<float, float>(this.Latitude, this.Longitude);
-        }
-
-        /// <summary> Converts a string number to a degree value. </summary>
-        /// <returns> The degree value. </returns>
-        /// <param name="Val"> The string value. </param>
-        private float RawToDeg(string Val)
-        {
-            string[] GPSplit = Val.Split('.');
-            //Console.WriteLine(GPSplit[0]);
-            float Deg = float.Parse(GPSplit[0].Substring(0, GPSplit[0].Length - 2));
-            float Min = float.Parse(GPSplit[0].Substring(GPSplit[0].Length - 2) + '.' + GPSplit[1]);
-            return Deg + Min / 60.0f;
-        }
+        public void UpdateState() { }// => GetCoords();
 
         public DataUnit GetData()
         {
             return new DataUnit("MTK3339")
             {
-                { "HasFix", HasFix() }, // TODO: This needs to be stored from last reading instead of re-checked.
-                { "Lat", this.Latitude},
-                { "Lon", this.Longitude}
+                //{ "HasFix", HasFix() }, // TODO: This needs to be stored from last reading instead of re-checked.
+                //{ "Lat", this.Latitude},
+               // { "Lon", this.Longitude}
             }
             .SetSystem(this.System);
         }
---
+++ UnitTest/ComponentTesting/MTK3339Tests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Scarlet.Components.Sensors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest.ComponentTesting
+{
+    [TestClass]
+    public class MTK3339Tests
+    {
+        [TestMethod]
+        public void TestDataConvert()
+        {
+            MTK3339 Sens = new MTK3339(null);
+            PrivateObject SensAccess = new PrivateObject(Sens);
+
+            byte? Checksum = (byte?)SensAccess.Invoke("GetChecksum", "$GPRMC,012600.089,V,,,,,0.00,0.00,110618,,,N*46", false);
+            Assert.IsNotNull(Checksum);
+            Assert.AreEqual((byte?)0x46, Checksum);
+
+            Checksum = (byte?)SensAccess.Invoke("GetChecksum", "$GPGSA,A,1,,,,,,,,,,,,,,,*1E", false);
+            Assert.IsNotNull(Checksum);
+            Assert.AreEqual((byte?)0x1E, Checksum);
+
+            Checksum = (byte?)SensAccess.Invoke("GetChecksum", "GPVTG,0.00,T,,M,0.00,N,0.00,K,N", true);
+            Assert.IsNotNull(Checksum);
+            Assert.AreEqual((byte?)0x32, Checksum);
+
+            Checksum = (byte?)SensAccess.Invoke("GetChecksum", "NotAValidPacket*1E", false);
+            Assert.IsNull(Checksum);
+
+            Checksum = (byte?)SensAccess.Invoke("GetChecksum", "$NotAValidPacket", false);
+            Assert.IsNull(Checksum);
+
+            Checksum = (byte?)SensAccess.Invoke("GetChecksum", null, false);
+            Assert.IsNull(Checksum);
+
+            Checksum = (byte?)SensAccess.Invoke("GetChecksum", "", false);
+            Assert.IsNull(Checksum);
+        }
+    }
+}
