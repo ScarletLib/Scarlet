818f62a78443641a599b8d7197ef5738624ca39a
Cai Biesinger <Cai.Biesinger@outlook.com>
2018-06-25 11:21:54 -0700

    Added TraceLogging, reduced complexity.

--- Components/Sensors/MQ135.cs
+++ Components/Sensors/MQ135.cs
@@ -12,10 +12,12 @@ namespace Scarlet.Components.Sensors
     public class MQ135 : ISensor
     {
         public string System { get; set; }
-        private IAnalogueIn Input;
-        private double RS, RL, SupplyVoltage;
+        public bool TraceLogging { get; set; }
+        private readonly IAnalogueIn Input;
+        private readonly double RS, RL;
+        private double SupplyVoltage;
         private int ResistanceCal = 41763;
-        private float LastReading, Temperature, Humidity;
+        private float LastReadingCal, LastReadingUncal, Temperature, Humidity;
 
         /* Typical circuit setup:
          * 
@@ -43,7 +45,7 @@ namespace Scarlet.Components.Sensors
         {
             return new DataUnit("MQ135")
             {
-                { "PPMReading", this.LastReading }
+                { "PPMReadingCal", this.LastReadingCal }
             }
             .SetSystem(this.System);
         }
@@ -51,21 +53,22 @@ namespace Scarlet.Components.Sensors
         /// <summary> Due to this being an analogue sensor, this simply tries to get a reading and sees if it is within range. </summary>
         public bool Test()
         {
-            try { GetReading(); }
+            float Output;
+            try { Output = GetReadingUncalibrated(); }
             catch (Exception) { return false; }
-            return true;
+            return Output != -1;
         }
 
-        public void UpdateState() => this.LastReading = GetReading();
+        public void UpdateState() => CalculatePPM();
 
         /// <summary> Gets the most recent sensor reading. </summary>
         /// <remarks> Currently, temperature/humidity calibration settings are not applied. </remarks>
         /// <returns> The estimated total ppm of pollutants in the air. </returns>
-        public float GetReading() => CalculatePPM(this.Input, this.RS, this.RL, this.SupplyVoltage, this.ResistanceCal, true, this.Humidity, this.Temperature);
+        public float GetReading() => this.LastReadingCal;
 
         /// <summary> Gets the most recent sensor reading, without applying temperature or humidity compensation. </summary>
         /// <returns> The estimated total ppm of pollutants in the air. </returns>
-        public float GetReadingUncalibrated() => CalculatePPM(this.Input, this.RS, this.RL, this.SupplyVoltage, this.ResistanceCal, false, 0, 0);
+        public float GetReadingUncalibrated() => this.LastReadingUncal;
 
         /// <summary> Applies some basic calibration to the sensor to compensate for air temperature. </summary>
         /// <remarks> Defaults to 20 C. </remarks>
@@ -87,19 +90,32 @@ namespace Scarlet.Components.Sensors
             this.Humidity = Humidity;
         }
 
-        /// <summary> Applies calibration for base sensor resistance. This is probably constant for each individual sensor. </summary>
+        /// <summary> Applies calibration for base sensor resistance. This is probably constant for each individual sensor. Gets used in both calibrated and uncalibrated readings. </summary>
         /// <remarks> You'll need to put the sensor into a 100 ppm CO2 environment at 20C and 65% RH to get this value. 41763 is used as default. </remarks>
         /// <param name="Resistance"> The measured resistance of the sensor, in the specific calibration conditions as listed above. Should be near 42K. </param>
         public void CalibrateResistance(int Resistance) { this.ResistanceCal = Resistance; }
 
-        public static float CalculatePPM(IAnalogueIn Input, double RS, double RL, double SupplyVoltage, int ResistanceCal, bool UseCal, float Humidity, float Temperature)
+        /// <summary> Updates the current supply voltage used for readings. Gets used in both calibrated and uncalibrated readings. </summary>
+        /// <param name="SupplyVoltage"> The current supply voltage (should be close to 5V). </param>
+        public void CalibrateSupply(double SupplyVoltage) { this.SupplyVoltage = SupplyVoltage; }
+
+        /// <summary> Approximates pollutant PPM from sensor output. </summary>
+        /// <remarks> See comments at the top of this class to understand the expected electrical setup and values. </remarks>
+        private void CalculatePPM()
         {
-            double SensorResistance = ((RL * SupplyVoltage) / Input.GetInput()) - (RS + RL);
-            double ResistanceRatio = SensorResistance / ResistanceCal;
-            double CalibrationMult = (UseCal ? GetCalibrationMultipler(Humidity, Temperature) : 1.0);
-            Log.Output(Log.Severity.DEBUG, Log.Source.SENSORS, "MQ135 Calculated resistance: " + SensorResistance + ", Ratio: " + ResistanceRatio);
-            if (ResistanceRatio < 0.358 || ResistanceRatio > 2.428) { return -1; }
-            return (float)(116.6020682 * Math.Pow(ResistanceRatio, -2.769034857));
+            double SensorResistance = ((this.RL * this.SupplyVoltage) / this.Input.GetInput()) - (this.RS + this.RL);
+            double ResistanceRatio = SensorResistance / this.ResistanceCal;
+            if (this.TraceLogging) { Log.Trace(this, "Calculated resistance: " + SensorResistance + ", Ratio: " + ResistanceRatio); }
+            if (ResistanceRatio < 0.358 || ResistanceRatio > 2.428)
+            {
+                this.LastReadingUncal = -1;
+                this.LastReadingCal = -1;
+                return;
+            }
+            this.LastReadingUncal = (float)(116.6020682 * Math.Pow(ResistanceRatio, -2.769034857));
+
+            // double CalibrationMult = (UseCal ? GetCalibrationMultipler(this.Humidity, this.Temperature) : 1.0);
+            // this.LastReadingCal = ...
         }
 
         // TODO: This still needs to be implemented.
