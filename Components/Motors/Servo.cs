0181d97fccb4e1e83cb3157beb9237998cf6f3ea
Cai Biesinger <Cai.Biesinger@outlook.com>
2018-07-22 19:52:55 -0700

    Working on Servo

--- Components/Motors/Servo.cs
+++ Components/Motors/Servo.cs
@@ -1,33 +1,44 @@
 ﻿using Scarlet.IO;
+using Scarlet.Utilities;
 using System;
 
 namespace Scarlet.Components.Motors
 {
     public class Servo : IServo
     {
-        private readonly IPWMOutput PWMOut;
         public int Position { get; private set; }
         public bool TraceLogging { get; set; }
 
+        private readonly IPWMOutput PWMOut;
+        private readonly float MaxTime, MinTime, PWMFreq;
+        private readonly int AngleRange;
         private bool Enabled;
 
-        public Servo(IPWMOutput PWMOut)
+        public Servo(IPWMOutput PWMOut, int AngleRange = 180, float MaxTime_ms = 2, float MinTime_ms = 1, float PWMFrequency = 50)
         {
             this.PWMOut = PWMOut;
-            this.PWMOut.SetFrequency(50); // TODO: Set this to an actual value, and check if this overrides others.
+            this.MaxTime = MaxTime_ms;
+            this.MinTime = MinTime_ms;
+            this.PWMFreq = PWMFrequency;
+            this.AngleRange = AngleRange;
+            this.PWMOut.SetFrequency(this.PWMFreq);
         }
 
         public void SetEnabled(bool Enabled)
         {
-            if (!Enabled) { SetPosition(this.Position); }
             this.Enabled = Enabled;
+            this.PWMOut.SetEnabled(Enabled);
         }
 
         public void SetPosition(int NewPosition)
         {
-            if (this.Position == NewPosition) { return; }
-            // TODO: Do filtering
             this.Position = NewPosition;
+            // TODO: Do filtering
+            NewPosition = Math.Min(this.AngleRange, Math.Max(0, NewPosition)); // Caps to 0 -> AngleRange
+            float OnTime_ms = this.MinTime + ((this.MaxTime - this.MinTime) * (1 - ((this.AngleRange - NewPosition) * 1F / this.AngleRange)));
+            float Output = OnTime_ms / (1000F / this.PWMFreq);
+            this.PWMOut.SetOutput(Output);
+            if (this.TraceLogging) { Log.Trace(this, "Servo moved to " + NewPosition + " degrees, which is " + (Output * 100).ToString("F2") + "% on time (" + OnTime_ms + "ms)"); }
         }
 
         private void SetPositionDirectly(int NewPosition)
