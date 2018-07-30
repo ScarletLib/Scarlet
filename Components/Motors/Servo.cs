ba314dd27d14417427818f40d3b89400e522a497
Cai Biesinger <Cai.Biesinger@outlook.com>
2018-07-30 15:54:44 -0700

    Added documentation, this was supposed to be in the last commit...

--- Components/Motors/Servo.cs
+++ Components/Motors/Servo.cs
@@ -14,6 +14,13 @@ namespace Scarlet.Components.Motors
         private readonly int AngleRange;
         private bool Enabled;
 
+        /// <summary> Drives a basic PWM-controlled servo motor. </summary>
+        /// <remarks> Servos usually don't care about the PWM frequency, but about the on-time of the pulses. Therefore, the range is configured with on-times instead of PWM percentage. </remarks>
+        /// <param name="PWMOut"> The PWM output to use to control the servo. </param>
+        /// <param name="AngleRange"> The range of the servo (check the datasheet). Usually between 180 and 300. </param>
+        /// <param name="MaxTime_ms"> The on-time (in milliseconds) that corresponds with the maximum servo displacement. Usually between 1 and 3. </param>
+        /// <param name="MinTime_ms"> The on-time (in milliseconds) that corresponds with the minimum servo displacement. Less than <see cref="MaxTime_ms"/>, usually between 0.5 and 2. </param>
+        /// <param name="PWMFrequency"> The frequency to set the PWM output to. Usually between 10 and 200. </param>
         public Servo(IPWMOutput PWMOut, int AngleRange = 180, float MaxTime_ms = 2, float MinTime_ms = 1, float PWMFrequency = 50)
         {
             this.PWMOut = PWMOut;
@@ -24,12 +31,16 @@ namespace Scarlet.Components.Motors
             this.PWMOut.SetFrequency(this.PWMFreq);
         }
 
+        /// <summary> Enables/disables output to the servo. Disabling usually causes it to stop resisting external movement. </summary>
+        /// <param name="Enabled"> Whether to output a PWM signal to the servo. </param>
         public void SetEnabled(bool Enabled)
         {
             this.Enabled = Enabled;
             this.PWMOut.SetEnabled(Enabled);
         }
 
+        /// <summary> Changes the servo's position to the given angle. </summary>
+        /// <param name="NewPosition"> The desired angle, in degrees. Input outside the range 0->AngleRange is capped at those limits. </param>
         public void SetPosition(int NewPosition)
         {
             this.Position = NewPosition;
