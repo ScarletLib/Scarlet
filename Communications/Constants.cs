c04091f96045a4042d740f7d134230d60f9cee8c
Cai Biesinger <Cai.Biesinger@outlook.com>
2018-08-25 15:14:29 -0700

    Step 1: Toss out the old code.

--- Communications/Constants.cs
+++ Communications/Constants.cs
@@ -1,34 +1,5 @@
 ﻿namespace Scarlet.Communications
 {
-    /// <summary> Represents data type. </summary>
-    public enum TypeID
-    {
-        BOOL = 0,
-        CHAR = 1,
-        DOUBLE = 2,
-        FLOAT = 3,
-        INT = 4,
-        LONG = 5,
-        SHORT = 6,
-        UINT = 7,
-        ULONG = 8,
-        USHORT = 9,
-        BYTE = 10,
-        STRING = 11,
-        BYTES = 12,
-        MAX = 13,
-    }
-
-    /// <summary> Represents the priority of packet, from highest to lowest. </summary>
-    public enum PacketPriority
-    {
-        USE_DEFAULT = -1,
-        EMERGENT = 0,
-        HIGH = 1,
-        MEDIUM = 2,
-        LOW = 3,
-        LOWEST = 4
-    }
 
     static class Constants
     {
