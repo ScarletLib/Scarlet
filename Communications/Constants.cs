ea951e63089ca28a71dea254e8c0899716c9d4b5
Cai Biesinger <Cai.Biesinger@outlook.com>
2018-08-25 15:21:20 -0700

    Adding new packet IDs

--- Communications/Constants.cs
+++ Communications/Constants.cs
@@ -9,7 +9,12 @@
         #endregion
 
         #region Reserved Packet IDs
-        public const int WATCHDOG_PING = 0xF0;
+        public const byte WATCHDOG_FROM_SERVER = 0xF0;
+        public const byte WATCHDOG_FROM_CLIENT = 0xF1;
+        public const byte BUFFER_LENGTH_CHANGE = 0xF2;
+        public const byte TIME_SYNCHRONIZATION = 0xF3;
+        public const byte HANDSHAKE_FROM_CLIENT = 0xF4;
+        public const byte HANDSHAKE_FROM_SERVER = 0xF5;
         #endregion
 
     }
