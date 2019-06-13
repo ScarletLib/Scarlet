010abaa3035078b5bd6203af6a1901d5a91c1962
Cai Biesinger <cai.biesinger@outlook.com>
2019-06-13 15:52:33 -0700

    Building and releasing Scarlet 0.6.0!

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Scarlet")]
+[assembly: AssemblyTitle("HuskyRobotics.Scarlet")]
 [assembly: AssemblyDescription("Robotics Library by Cai and Jaden")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Husky Robotics")]
-[assembly: AssemblyProduct("Scarlet")]
+[assembly: AssemblyProduct("HuskyRobotics.Scarlet")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.5.2.0")]
-[assembly: AssemblyFileVersion("0.5.2.0")]
+[assembly: AssemblyVersion("0.6.0.0")]
+[assembly: AssemblyFileVersion("0.6.0.0")]
